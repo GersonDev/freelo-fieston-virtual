df3def3858bab183497e68dc2428fd2ddb23a362
Carlos Leonardo Camilo Vargas Huaman <carlos.vargas.huaman@avantica.net>
2020-06-09 00:32:56 -0500

    * Improve code verification screen * improve flow

--- app/src/main/java/com/spydevs/fiestonvirtual/ui/main/MainActivity.kt
+++ app/src/main/java/com/spydevs/fiestonvirtual/ui/main/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.spydevs.fiestonvirtual.R
+import com.spydevs.fiestonvirtual.ui.welcome.WelcomeDialogFragment
 import kotlinx.android.synthetic.main.toolbar_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,10 +27,21 @@ class MainActivity : AppCompatActivity() {
 //        shoppingCartViewModel.requestCart()
 //        initBadge()
 
-
-
         val navView: BottomNavigationView = findViewById(R.id.mainBottomNavigationView)
         val navController = findNavController(R.id.nav_host_fragment)
         navView.setupWithNavController(navController)
+
+        showWelcomeScreen()
+    }
+
+    private fun showWelcomeScreen() {
+        //TODO delete data test.
+        WelcomeDialogFragment.newInstance(
+            "BIENVENIDO JUAN",
+            "Hoy celebramos el aniversario 50 de Super Mercados metro"
+            ,
+            "Un viaje para dos personas a Cancun",
+            "https://image.freepik.com/vector-gratis/caja-regalo-sorpresa-abierta_3446-340.jpg"
+        ).show(supportFragmentManager, WelcomeDialogFragment.TAG)
     }
 }
