b50651704a974697dfbbd828ef7240c366caf165
Bill Flores <bilizen3@gmail.com>
2020-06-25 23:27:30 -0500

    add gallery feature

--- app/src/main/java/com/spydevs/fiestonvirtual/ui/main/gallery/GalleryFragment.kt
+++ app/src/main/java/com/spydevs/fiestonvirtual/ui/main/gallery/GalleryFragment.kt
@@ -1,11 +1,40 @@
-package com.spydevs.fiestonvirtual.ui.main.chat
+package com.spydevs.fiestonvirtual.ui.main.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.spydevs.fiestonvirtual.R
-
+import com.spydevs.fiestonvirtual.ui.main.gallery.photo.PhotoAdapter
+import kotlinx.android.synthetic.main.fragment_gallery.*
+import org.koin.android.ext.android.inject
 
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
-    private lateinit var galleryViewModel: GalleryViewModel
+    private val galleryViewModel: GalleryViewModel by inject()
+    private val photoAdapter: PhotoAdapter by lazy {
+        PhotoAdapter {
+
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpGalleryList()
+        subscribeToPhotoList()
+        galleryViewModel.getPhotoList()
+    }
+
+    private fun setUpGalleryList() {
+        galleryFragment_rv.apply {
+            adapter = this@GalleryFragment.photoAdapter
+        }
+    }
+
+    private fun subscribeToPhotoList() {
+        this.galleryViewModel.photoList.observe(viewLifecycleOwner, Observer {
+            this@GalleryFragment.photoAdapter.addData(it.toMutableList())
+        })
+    }
 
 }
