b76e71464b8231ba9a5f57077284d3f1c76b2ea7
Bill Flores <bilizen3@gmail.com>
2020-07-10 18:32:33 -0500

    add unit test to GalleryViewModelTest

--- app/src/main/java/com/spydevs/fiestonvirtual/ui/main/gallery/GalleryResult.kt
+++ app/src/main/java/com/spydevs/fiestonvirtual/ui/main/gallery/GalleryResult.kt
@@ -4,7 +4,7 @@ import com.spydevs.fiestonvirtual.domain.models.error.ErrorResponse
 import com.spydevs.fiestonvirtual.domain.models.gallery.GalleryItem
 
 sealed class GalleryResult {
-    class Loading(val show: Boolean) : GalleryResult()
-    class GetGallerySuccessful(val galleryItemList: List<GalleryItem>) : GalleryResult()
-    class GetGalleryError(val errorResponse: ErrorResponse) : GalleryResult()
+    data class Loading(val show: Boolean) : GalleryResult()
+    data class GetGallerySuccessful(val galleryItemList: List<GalleryItem>) : GalleryResult()
+    data class GetGalleryError(val errorResponse: ErrorResponse) : GalleryResult()
 }
