5fce29a2a342a021d22650bb2e5438052866f5a1
Bill Flores <bilizen3@gmail.com>
2020-08-01 16:26:11 -0500

    add the type video view holder

---
+++ app/src/main/java/com/spydevs/fiestonvirtual/ui/main/gallery/adapter/GalleryItemVideoViewHolder.kt
@@ -0,0 +1,25 @@
+package com.spydevs.fiestonvirtual.ui.main.gallery.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.spydevs.fiestonvirtual.domain.models.gallery.GalleryItem
+import com.spydevs.fiestonvirtual.util.extensions.loadUrl
+import kotlinx.android.synthetic.main.layout_item_video.view.*
+
+class GalleryItemVideoViewHolder(
+    private val view: View,
+    private val onVideoClickListener: (position: Int) -> Unit
+) : RecyclerView.ViewHolder(view) {
+
+    init {
+        this.view.videoItemLayout_cv.setOnClickListener {
+            onVideoClickListener(adapterPosition)
+        }
+    }
+
+    fun bind(photo: GalleryItem) {
+        photo.file.let {
+            this.view.videoItemLayout_iv.loadUrl(it)
+        }
+    }
+}
