53af917d9a5cd4ecd5971f0e4e907b7df6c4b4ea
Bill Flores <bilizen3@gmail.com>
2020-08-08 19:29:03 -0500

    add di to make like

--- app/src/main/java/com/spydevs/fiestonvirtual/di/RepositoriesModule.kt
+++ app/src/main/java/com/spydevs/fiestonvirtual/di/RepositoriesModule.kt
@@ -1,17 +1,7 @@
 package com.spydevs.fiestonvirtual.di
 
-import com.spydevs.fiestonvirtual.data.repository.CodeRepositoryImpl
-import com.spydevs.fiestonvirtual.data.repository.EventRepositoryImpl
-import com.spydevs.fiestonvirtual.data.repository.TriviaRepositoryImpl
-import com.spydevs.fiestonvirtual.data.repository.GalleryRepositoryImpl
-import com.spydevs.fiestonvirtual.data.repository.UsersRepositoryImpl
-import com.spydevs.fiestonvirtual.data.repository.CommentRepositoryImpl
-import com.spydevs.fiestonvirtual.domain.repository.CodeRepository
-import com.spydevs.fiestonvirtual.domain.repository.EventRepository
-import com.spydevs.fiestonvirtual.domain.repository.TriviaRepository
-import com.spydevs.fiestonvirtual.domain.repository.GalleryRepository
-import com.spydevs.fiestonvirtual.domain.repository.UsersRepository
-import com.spydevs.fiestonvirtual.domain.repository.CommentRepository
+import com.spydevs.fiestonvirtual.data.repository.*
+import com.spydevs.fiestonvirtual.domain.repository.*
 import org.koin.dsl.module
 
 val repositoryModule = module {
@@ -21,4 +11,5 @@ val repositoryModule = module {
     single<TriviaRepository> { TriviaRepositoryImpl(get()) }
     single<GalleryRepository> { GalleryRepositoryImpl(get()) }
     single<CommentRepository> { CommentRepositoryImpl(get()) }
+    single<LikeRepository> { LikeRepositoryImpl(get()) }
 }
