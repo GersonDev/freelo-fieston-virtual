b2135fd797a2a9cb0c277c71a9e7e0b5f4db45b8
Bill Flores <bilizen3@gmail.com>
2020-08-08 19:28:12 -0500

    update make like to domain module

--- domain/src/main/java/com/spydevs/fiestonvirtual/domain/usecases/abstractions/like/MakeLikeUseCase.kt
+++ domain/src/main/java/com/spydevs/fiestonvirtual/domain/usecases/abstractions/like/MakeLikeUseCase.kt
@@ -1,5 +1,11 @@
 package com.spydevs.fiestonvirtual.domain.usecases.abstractions.like
 
+import com.spydevs.fiestonvirtual.domain.models.error.ErrorResponse
+import com.spydevs.fiestonvirtual.domain.models.like.MakeLikeResponse
+import com.spydevs.fiestonvirtual.domain.resource.ResultType
+
 interface MakeLikeUseCase {
-    suspend operator fun invoke(idPost: Int)
+    suspend operator fun invoke(
+        idPost: Int
+    ): ResultType<MakeLikeResponse, ErrorResponse>
 }
--- domain/src/main/java/com/spydevs/fiestonvirtual/domain/usecases/implementations/like/MakeLikeUseCaseImpl.kt
+++ domain/src/main/java/com/spydevs/fiestonvirtual/domain/usecases/implementations/like/MakeLikeUseCaseImpl.kt
@@ -1,8 +1,11 @@
 package com.spydevs.fiestonvirtual.domain.usecases.implementations.like
 
+import com.spydevs.fiestonvirtual.domain.models.error.ErrorResponse
 import com.spydevs.fiestonvirtual.domain.models.like.MakeLikeRequest
+import com.spydevs.fiestonvirtual.domain.models.like.MakeLikeResponse
 import com.spydevs.fiestonvirtual.domain.repository.LikeRepository
 import com.spydevs.fiestonvirtual.domain.repository.UsersRepository
+import com.spydevs.fiestonvirtual.domain.resource.ResultType
 import com.spydevs.fiestonvirtual.domain.usecases.abstractions.like.MakeLikeUseCase
 
 class MakeLikeUseCaseImpl(
@@ -10,8 +13,10 @@ class MakeLikeUseCaseImpl(
     private val likeRepository: LikeRepository
 ) : MakeLikeUseCase {
 
-    override suspend fun invoke(idPost: Int) {
-        likeRepository.makeLike(
+    override suspend fun invoke(
+        idPost: Int
+    ): ResultType<MakeLikeResponse, ErrorResponse> {
+        return likeRepository.makeLike(
             MakeLikeRequest(
                 usersRepository.getLocalUser().id,
                 idPost
