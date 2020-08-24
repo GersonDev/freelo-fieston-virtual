b72bd7286fd0f10796a366943fe9ac3053531ae2
Bill Flores <bilizen3@gmail.com>
2020-08-23 23:58:27 -0500

    refactor MyFirebaseMessagingService

--- app/src/main/java/com/spydevs/fiestonvirtual/services/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/spydevs/fiestonvirtual/services/fcm/MyFirebaseMessagingService.kt
@@ -6,22 +6,27 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.spydevs.fiestonvirtual.R
+import com.spydevs.fiestonvirtual.domain.usecases.abstractions.notifications.SendTokenFirebaseUseCase
 import com.spydevs.fiestonvirtual.ui.main.MainActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 
-//TODO refactor class
-class MyFirebaseMessagingService : FirebaseMessagingService() {
+class MyFirebaseMessagingService : FirebaseMessagingService(), KoinComponent {
+
+    private val sendTokenFirebaseUseCase: SendTokenFirebaseUseCase by inject()
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
         remoteMessage.notification?.let {
-            sendNotification(it.title, it.body)
+            createNotification(it.title, it.body)
         }
     }
 
@@ -29,26 +34,17 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         sendRegistrationToServer(token)
     }
 
-    private fun sendRegistrationToServer(token: String?) {
-        val oneTimeWorkRequest = OneTimeWorkRequest
-            .Builder(SendTokenWorker::class.java)
-            .setInputData(
-                Data.Builder()
-                    .putString(SendTokenWorker.TOKEN, token)
-                    .build()
-            )
-            .build()
-
-        WorkManager
-            .getInstance(applicationContext)
-            .beginWith(oneTimeWorkRequest)
-            .enqueue()
+    private fun sendRegistrationToServer(token: String) {
+        GlobalScope.launch {
+            sendTokenFirebaseUseCase(token)
+        }
     }
 
-    private fun sendNotification(title: String?, messageBody: String?) {
-        val intent = Intent(this, MainActivity::class.java).apply {
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-        }
+    private fun createNotification(title: String?, messageBody: String?) {
+        val intent = Intent(this, MainActivity::class.java)
+            .apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            }
         val pendingIntent = PendingIntent.getActivity(
             this,
             0,
@@ -57,33 +53,62 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         )
 
         val channelId = getString(R.string.notification_default_channel_id)
-        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
-        val notificationBuilder = NotificationCompat
-            .Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle(title)
-            .setContentText(messageBody)
-            .setStyle(NotificationCompat.BigTextStyle().bigText(messageBody))
-            .setAutoCancel(true)
-            .setSound(defaultSoundUri)
-            .setContentIntent(pendingIntent)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
+        val notificationBuilder = createNotificationDefault(
+            title,
+            messageBody,
+            channelId,
+            RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION),
+            pendingIntent
+        )
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                getString(R.string.notification_default_channel_name),
-                NotificationManager.IMPORTANCE_DEFAULT
-            ).apply {
-                description = getString(R.string.notification_default_channel_description)
-            }
-            notificationManager.createNotificationChannel(channel)
+            notificationManager.createNotificationChannel(
+                createChannelDefault(
+                    channelId,
+                    getString(R.string.notification_default_channel_name),
+                    getString(R.string.notification_default_channel_description)
+                )
+            )
         }
-
         notificationManager.notify(0, notificationBuilder.build())
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createChannelDefault(
+        channelId: String,
+        name: String,
+        description: String
+    ): NotificationChannel {
+        return NotificationChannel(
+            channelId,
+            name,
+            NotificationManager.IMPORTANCE_DEFAULT
+        ).apply {
+            this.description = description
+        }
+    }
+
+    private fun createNotificationDefault(
+        title: String?,
+        messageBody: String?,
+        channelId: String,
+        defaultSoundUri: Uri,
+        pendingIntent: PendingIntent
+    ): NotificationCompat.Builder {
+        return NotificationCompat
+            .Builder(this, channelId)
+            .apply {
+                setSmallIcon(R.mipmap.ic_launcher)
+                setContentTitle(title)
+                setContentText(messageBody)
+                setStyle(NotificationCompat.BigTextStyle().bigText(messageBody))
+                setAutoCancel(true)
+                setSound(defaultSoundUri)
+                setContentIntent(pendingIntent)
+                priority = NotificationCompat.PRIORITY_DEFAULT
+            }
+
+    }
 }
